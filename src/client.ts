1a06b37a23d3a5786940e330415fa49867eedf33
weber77 <weberkanga77@gmail.com>
2024-08-05 23:43:05 +0100

    intergrated sdk key generation

--- src/client.ts
+++ src/client.ts
@@ -6,63 +6,63 @@ import { ApiResponse, ZoomTokenResponse } from "./types";
  * Client class for interacting with the API.
  */
 export class LT {
-  private httpClient: AxiosInstance;
-  private baseURL: string;
-  private apiKey: string;
+    private httpClient: AxiosInstance;
+    private baseURL: string;
+    private apiKey: string;
 
-  private zoomHttpClient: AxiosInstance;
-  private zoomAccessToken: string | null = null;
-  /**
-   * Creates an instance of the Client.
-   * @param {string} baseURL - The base URL of the API.
-   */
-  constructor(apiKey: string) {
-    this.baseURL = "";
-    this.apiKey = apiKey;
-    this.httpClient = axios.create({
-      baseURL: "",
-      headers: { Authorization: `Bearer ${this.apiKey}` },
-    });
+    private zoomHttpClient: AxiosInstance;
+    private zoomAccessToken: string | null = null;
+    /**
+     * Creates an instance of the Client.
+     * @param {string} baseURL - The base URL of the API.
+     */
+    constructor(apiKey: string) {
+        this.baseURL = "";
+        this.apiKey = apiKey;
+        this.httpClient = axios.create({
+            baseURL: "",
+            headers: { Authorization: `Bearer ${this.apiKey}` },
+        });
 
-    this.zoomHttpClient = axios.create({
-      baseURL: "https://api.zoom.us/v2/",
-    });
-  }
-
-  /**
-   * Authenticates with the API and stores the access token for future requests.
-   * @returns {Promise<ApiResponse<ZoomTokenResponse>>} Returns an instance of the Client.
-   * @param {string} clientId - The clientId of the Zoom API.
-   * @param {string} clientSecret - The clientSecret of the Zoom API.
-   */
-  async authenticateZoom(
-    clientId: string,
-    clientSecret: string
-  ): Promise<ApiResponse<ZoomTokenResponse>> {
-    const tokenUrl = "https://zoom.us/oauth/token";
-    const authString = Buffer.from(`${clientId}:${clientSecret}`).toString(
-      "base64"
-    );
-
-    const data = 'grant_type=client_credentials';
-
-    try {
-      const response = await axios.post<ZoomTokenResponse>(tokenUrl, data, {
-        headers: {
-          Authorization: `Basic ${authString}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      });
-
-      this.zoomAccessToken = response.data.access_token;
-
-      return {
-        data: response.data,
-        status: response.status,
-        statusText: response.statusText,
-      };
-    } catch (error: any) {
-      throw new Error(error.response?.data?.message || error.message);
+        this.zoomHttpClient = axios.create({
+            baseURL: "https://api.zoom.us/v2/",
+        });
     }
-  }
+
+    /**
+     * Authenticates with the API and stores the access token for future requests.
+     * @returns {Promise<ApiResponse<ZoomTokenResponse>>} Returns an instance of the Client.
+     * @param {string} clientId - The clientId of the Zoom API.
+     * @param {string} clientSecret - The clientSecret of the Zoom API.
+     */
+    async authenticateZoom(
+        clientId: string,
+        clientSecret: string
+    ): Promise<ApiResponse<ZoomTokenResponse>> {
+        const tokenUrl = "https://zoom.us/oauth/token";
+        const authString = Buffer.from(`${clientId}:${clientSecret}`).toString(
+            "base64"
+        );
+
+        const data = 'grant_type=client_credentials';
+
+        try {
+            const response = await axios.post<ZoomTokenResponse>(tokenUrl, data, {
+                headers: {
+                    Authorization: `Basic ${authString}`,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
+
+            this.zoomAccessToken = response.data.access_token;
+
+            return {
+                data: response.data,
+                status: response.status,
+                statusText: response.statusText,
+            };
+        } catch (error: any) {
+            throw new Error(error.response?.data?.message || error.message);
+        }
+    }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,52 @@
 import { AxiosInstance } from "axios";
 
 export interface ApiResponse<T> {
-  data: T;
-  status: number;
-  statusText: string;
+    data: T;
+    status: number;
+    statusText: string;
 }
 
 export interface ZoomTokenResponse {
-  access_token: string;
-  token_type: string;
-  expires_in: number;
+    access_token: string;
+    token_type: string;
+    expires_in: number;
 }
 
-export interface User {
-  id: number;
-  name: string;
-  email: string;
+
+export interface RegisterInstitutionBody {
+    id: number;
+    name: string;
+    email: string;
+    publicAddress: string;
+    password: string;
+    latitude: string;
+    longitude: string;
+}
+
+
+export interface RegisterInstitutionResponse {
+    id: number;
+    name: string;
+    email: string;
+    token: null | string;
+    createdAt: string;
+    updatedAt?: string;
+}
+
+export interface LoginInstitutionResponse {
+    id: number;
+    name: string;
+    email: string;
+    publicAddress: string;
+    token: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface LoginInstitutionBody {
+    email: string,
+    password: string,
+    type: string
 }
 
